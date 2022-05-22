20d7e6ed577e8dfc525ac97f5a456caed22d77f9
Bartosz Kot <bartosz.kot.00@gmail.com>
2022-05-22 23:00:04 +0200

    Day 11 of 30 Days of Python

--- day_11/functions.py
+++ day_11/functions.py
@@ -3,6 +3,7 @@
 # LEVEL 1
 
 import math
+from string import digits
 
 
 def add_two_numbers(a,b):
@@ -67,3 +68,74 @@ def remove_item(lst:list, item):
     '''Exercise 12'''
     lst.remove(item)
     return lst
+
+def sum_of_numbers(num:int):
+    '''Exercise 13'''
+    return sum([i for i in range(num+1)])
+
+def sum_of_odds(num:int):
+    '''Exercise 14'''
+    return sum([i for i in range(num+1) if i % 2 != 0])
+
+def sum_of_evens(num:int):
+    '''Exercise 15'''
+    return sum([i for i in range(num+1) if i % 2 == 0])
+
+# LEVEL 2
+
+def evens_and_odds(num:int):
+    '''Exercise 1'''
+    if num < 1:
+        return "Number must be positive!"
+    evens = len([i for i in range(num+1) if i % 2 == 0])
+    odds = len([i for i in range(num+1) if i % 2 != 0])
+    return f"The number of evens is {evens} and of odds is {odds}."
+
+def factorial(num:int):
+    '''Exercise 2'''
+    fct = 1
+    for i in range(1,num+1):
+        fct *= i
+    return fct
+
+def is_empty(parameter):
+    '''Exercise 3'''
+    return True if len(parameter) == 0 else False
+
+# LEVEL 3
+
+def is_prime(num):
+    '''Exercise 1'''
+    for i in range(2,num):
+        if num % i == 0:
+            return False
+    return True
+
+def is_unique(lst:list):
+    '''Exercise 2'''
+    tmp_lst = []
+    for item in lst:
+        if item in tmp_lst:
+            return False
+        tmp_lst.append(item)
+    return True
+
+def same_type(lst:list):
+    '''Exercise 3'''
+    prev = lst[0]
+    for i in lst:
+        if type(i) != type(prev):
+            return False
+        prev = i
+    return True
+
+import keyword
+def valid_variable(var):
+    '''Exercise 4'''
+    digits
+    if var in keyword.kwlist:
+        return False
+    if var[0] in digits:
+        return False
+    return True
+
